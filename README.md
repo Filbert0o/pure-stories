12836868db3812e60a7dfe0d99b86562d6647d8d
Timothy Andrew Filbert <timfilbert97@gmail.com>
2019-03-02 16:56:05 -0500

    set up MEAN stack

---
+++ README.md
@@ -0,0 +1,13 @@
+# pure-stories
+Pure Stories
+
+setup
+
+```
+git clone 
+cd pure-stories
+npm install && cd client && npm install && cd ..
+```
+
+create .env file with MONGO_URI
+add secrets 
---
+++ backend/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config({path: '../.env'})
+
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+// import passport from 'passport';
+import path from 'path';
+import logger from 'morgan';
+import cors from 'cors';
+
+
+import busboy from 'connect-busboy';
+import busboyBodyParser from 'busboy-body-parser';
+
+// Import routes here
+// import users from './controllers/users';
+
+// Create our app instances
+const app = express();
+
+// Set our port to either a predetermined port number if you have set it up, or 3001
+const API_PORT = process.env.API_PORT || 3001;
+
+mongoose.connect(process.env.MONGO_URI)
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Now we should configure the API to use bodyParser and look for JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(cors());
+app.use(busboy());
+app.use(busboyBodyParser());
+
+// Passport middleware
+// app.use(passport.initialize());
+
+// Hello World at '/'
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+//Use Multiple Routes
+// app.use('/api/users', users);
+
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
